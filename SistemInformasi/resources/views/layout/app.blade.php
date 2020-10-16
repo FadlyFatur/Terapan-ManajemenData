7f208ef73361333767a8b4cc32c3420a67c92ffc
FadlyFTR <49541723+FadlyFTR@users.noreply.github.com>
2020-10-16 10:57:40 +0700

    Adding bootstrap & layout
    Mendapatkan bootstrap offline, menseting navbar, sidebar dan layouting awal

--- SistemInformasi/resources/views/layout/app.blade.php
+++ SistemInformasi/resources/views/layout/app.blade.php
@@ -1,21 +1,37 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
-    <title>Hello, world!</title>
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="{{ URL::asset('css/layout.css') }}">
+    @yield('css')
   </head>
+
   <body>
-    <h1>Hello, world!</h1>
+  @include('layout.nav')
 
-    <!-- Optional JavaScript; choose one of the two! -->
+  <div class="container-fluid">
+    <div class="row">
+      @include('layout.sidebar')
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="js/bootstrap.min.js"></script>
+      @yield('isi')
+
+    </div>
+</div>
+
+ <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+ <script src="{{asset('jquery-3.5.1.slim.min.js')}}" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+  <script src="{{asset('bootstrap.bundle.min.js')}}" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <script src="{{ URL::asset('js/app  .css') }}"></script>
+
+  @yield('js')
   </body>
 </html>
