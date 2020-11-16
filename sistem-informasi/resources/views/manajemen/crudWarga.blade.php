632d55e7556eaf27c04fd371f0ace6806724f6be
Cindy Mawar Kasih <49541521+Cindymawark@users.noreply.github.com>
2020-11-16 19:30:23 +0700

    Edit data

--- sistem-informasi/resources/views/manajemen/crudWarga.blade.php
+++ sistem-informasi/resources/views/manajemen/crudWarga.blade.php
@@ -69,10 +69,10 @@
 <!-- Modal Edit-->
   @foreach ($wargas as $a)
     <div class="modal fade" id="edit-{{$a['id']}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg " role="document">
+        <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -176,7 +176,7 @@
           </div>
         <div class="modal-footer">
           <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-          <button type="submit" class="btn btn-primary">Update!</button>
+          <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
         </div>
         </form>
         </div>
@@ -190,7 +190,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
