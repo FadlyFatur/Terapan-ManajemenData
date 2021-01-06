0ea9af3c27afe5a34bc16536a8a03902891347c1
FadlyFTR <49541723+FadlyFTR@users.noreply.github.com>
2021-01-06 19:10:14 +0700

    db seed config

--- sistem-informasi/database/seeds/DatabaseSeeder.php
+++ sistem-informasi/database/seeds/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
         // $this->call(userTableSeeder::class);
         // factory(App\User::class, 10)->create();
         // factory(App\staff::class, 10)->create();
-        // factory(App\warga::class, 15)->create();
-        // factory(App\acara::class, 25)->create();
-        // factory(App\beranda::class)->create();
+        factory(App\warga::class, 15)->create();
+        factory(App\acara::class, 25)->create();
+        factory(App\beranda::class)->create();
 
         $this->call(jabatanSeeder::class);
         $this->call(KerjaSeeder::class);
